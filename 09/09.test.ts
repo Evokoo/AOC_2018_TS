5d5dcc5a0c2e29445ac0b7dfaf70d789b9a47133
Evokoo <evanjohncooper@gmail.com>
2024-12-27 16:03:59 +1100

    Day 09 complete

--- 09/09.test.ts
+++ 09/09.test.ts
@@ -8,21 +8,21 @@ const currentDay = path.basename(Deno.cwd());
 describe(`AOC 2018 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		it("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(0);
+			expect(solveA("example_a", currentDay)).toBe(146373);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveA("input", currentDay)).toBe(412127);
 		});
 	});
 
-	describe.skip("Part B", () => {
+	describe("Part B", () => {
 		it("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(0);
+			expect(solveB("example_b", currentDay)).toBe(74765078);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveB("input", currentDay)).toBe(3482394794);
 		});
 	});
 });
--- 09/09.ts
+++ 09/09.ts
@@ -1,14 +1,51 @@
 // Imports
 import TOOLS from "tools";
+import { CircularLinkedList } from "./List.ts";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const { players, marbles } = parseInput(data);
+	return playGame(players, marbles);
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const { players, marbles } = parseInput(data);
+	return playGame(players, marbles * 100);
 }
 
+type Rules = { players: number; marbles: number };
+
 // Functions
+function parseInput(data: string): Rules {
+	const [players, marbles] = (data.match(/\d+/g) || []).map(Number);
+	return { players, marbles };
+}
+function playGame(players: number, marbles: number): number {
+	const scores: number[] = Array.from({ length: players }, () => 0);
+	const circle: CircularLinkedList<number> = new CircularLinkedList();
+
+	circle.append(0);
+
+	let currentNode = circle.head;
+
+	for (let marble = 1; marble <= marbles; marble++) {
+		if (marble % 23 === 0) {
+			//Move backwards
+			for (let i = 0; i < 6; i++) {
+				currentNode = currentNode!.previous;
+			}
+
+			scores[marble % players] += marble + currentNode!.data;
+			circle.delete(currentNode!);
+		} else {
+			// Move Forward
+			for (let i = 0; i < 2; i++) {
+				currentNode = currentNode!.next;
+			}
+			circle.insertAfter(currentNode!, marble);
+		}
+	}
+
+	return Math.max(...scores);
+}
