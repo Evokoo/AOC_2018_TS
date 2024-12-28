f9e33468872f2fb022b9f7d3aba460e005ddb8ab
Evokoo <evanjohncooper@gmail.com>
2024-12-28 17:01:36 +1100

    Day 12 complete

--- 12/12.test.ts
+++ 12/12.test.ts
@@ -8,21 +8,21 @@ const currentDay = path.basename(Deno.cwd());
 describe(`AOC 2018 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		it("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(0);
+			expect(solveA("example_a", currentDay)).toBe(325);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveA("input", currentDay)).toBe(3738);
 		});
 	});
 
-	describe.skip("Part B", () => {
+	describe("Part B", () => {
 		it("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(0);
+			expect(solveB("example_b", currentDay)).toBe(999999999374);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveB("input", currentDay)).toBe(3900000002467);
 		});
 	});
 });
--- 12/12.ts
+++ 12/12.ts
@@ -4,11 +4,87 @@ import TOOLS from "tools";
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const plantData = parseInput(data);
+	return simulatePlants(plantData, 20);
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const plantData = parseInput(data);
+	return simulatePlants(plantData, 1000);
 }
 
+type PlantData = { plants: Set<number>; notes: Map<string, string> };
+
 // Functions
+function parseInput(data: string): PlantData {
+	const sections = data.split("\n\n");
+	const initialState = sections[0].split(": ")[1];
+	const plants: Set<number> = new Set();
+	const notes: Map<string, string> = new Map();
+
+	for (let i = 0; i < initialState.length; i++) {
+		if (initialState[i] === "#") plants.add(i);
+	}
+
+	for (const line of sections[1].split("\n")) {
+		const [key, _, value] = line.split(" ");
+		notes.set(key, value);
+	}
+
+	return { plants, notes };
+}
+
+function simulatePlants({ plants, notes }: PlantData, generations: number) {
+	let currentState: Set<number> = plants;
+
+	for (let i = 0; i < generations; i++) {
+		const nextState: Set<number> = new Set();
+
+		for (const plant of currentState) {
+			const patterns = [
+				{ indices: [-4, -3, -2, -1, 0], offset: -2 },
+				{ indices: [-3, -2, -1, 0, 1], offset: -1 },
+				{ indices: [-2, -1, 0, 1, 2], offset: 0 },
+				{ indices: [-1, 0, 1, 2, 3], offset: 1 },
+				{ indices: [0, 1, 2, 3, 4], offset: 2 },
+			];
+
+			for (const { indices, offset } of patterns) {
+				const pattern = indices
+					.map((n) => (currentState.has(plant + n) ? "#" : "."))
+					.join("");
+
+				if (notes.has(pattern) && notes.get(pattern) === "#") {
+					nextState.add(plant + offset);
+				}
+			}
+		}
+
+		if (printState(currentState) === printState(nextState)) {
+			const nextScore = getScore(nextState);
+			const currentScore = getScore(currentState);
+			const difference = nextScore - currentScore;
+			return (50000000000 - (i + 1)) * difference + nextScore;
+		}
+
+		currentState = nextState;
+	}
+
+	return getScore(currentState);
+}
+function getScore(state: Set<number>): number {
+	return [...state].reduce((acc, cur) => acc + cur, 0);
+}
+function printState(state: Set<number>): string {
+	const indcies = [...state];
+	const max = Math.max(...indcies);
+	const min = Math.min(...indcies);
+
+	let plants = "";
+
+	for (let i = min; i < max; i++) {
+		plants += state.has(i) ? "#" : ".";
+	}
+
+	return plants;
+}
