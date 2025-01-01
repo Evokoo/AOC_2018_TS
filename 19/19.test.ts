99affe4b6a12f6995957585f4a9f2206f94b9e15
Evokoo <evanjohncooper@gmail.com>
2025-01-02 01:16:10 +1100

    Day 19 - Part 1 complete

--- 19/19.test.ts
+++ 19/19.test.ts
@@ -8,20 +8,20 @@ const currentDay = path.basename(Deno.cwd());
 describe(`AOC 2018 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		it("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(0);
+			expect(solveA("example_a", currentDay)).toBe(7);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveA("input", currentDay)).toBe(984);
 		});
 	});
 
-	describe.skip("Part B", () => {
-		it("Example", () => {
+	describe("Part B", () => {
+		it.skip("Example", () => {
 			expect(solveB("example_b", currentDay)).toBe(0);
 		});
 
-		it.skip("Solution", () => {
+		it("Solution", () => {
 			expect(solveB("input", currentDay)).toBe(0);
 		});
 	});
--- 19/19.ts
+++ 19/19.ts
@@ -1,14 +1,57 @@
 // Imports
 import TOOLS from "tools";
+import { Computer, Input } from "./Computer.ts";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	return runProgram(parseInput(data));
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	return runProgram(parseInput(data), true);
 }
 
+type Program = {
+	pointer: number;
+	inputs: Input[];
+};
+
 // Functions
+function parseInput(data: string): Program {
+	const lines: string[] = data.split("\n");
+	const pointer: number = Number(lines.shift()!.slice(-1));
+	const inputs: Input[] = [];
+
+	for (const line of lines) {
+		const [code, a, b, c] = line.split(" ");
+		inputs.push([code, +a, +b, +c]);
+	}
+
+	return { pointer, inputs };
+}
+function runProgram({ pointer, inputs }: Program, program2: boolean = false) {
+	const computer = new Computer(pointer);
+
+	if (program2) {
+		computer.setRegister = [1, 0, 0, 0, 0, 0, 0];
+	}
+
+	for (let i = 0; i < 10000; i++) {
+		if (computer.getPointer >= inputs.length) {
+			break;
+		}
+
+		computer.run(inputs[computer.getPointer]);
+
+		console.log({ i, register: computer.getRegister.join(",") });
+	}
+
+	// while (computer.getPointer < inputs.length) {
+	// 	computer.run(inputs[computer.getPointer]);
+
+	// 	console.log(computer.getRegister);
+	// }
+
+	return computer.getRegister[0];
+}
