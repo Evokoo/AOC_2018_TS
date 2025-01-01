3f882aa2bdde07e571b55b7c9dba84b1e3144ada
Evokoo <evanjohncooper@gmail.com>
2025-01-02 04:33:55 +1100

    Day 20 WIP

--- 20/20.ts
+++ 20/20.ts
@@ -4,6 +4,11 @@ import TOOLS from "tools";
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
+	const stack = parseInput(data);
+	// const tree = buildTree(stack);
+
+	console.log(stack);
+
 	return 0;
 }
 export function solveB(fileName: string, day: string): number {
@@ -11,4 +16,77 @@ export function solveB(fileName: string, day: string): number {
 	return 0;
 }
 
+type Node = {
+	steps: number;
+	parent: Node | null;
+	children: Node[];
+};
+
 // Functions
+function parseInput(data: string): (number | string)[] {
+	return (data.match(/\w+|\||\(|\)/g) || []).map(Number);
+}
+function buildTree(stack: string[]): Node {
+	function newNode(route: string, parent: Node | null = null): Node {
+		return {
+			steps: (parent?.steps ?? 0) + route.length,
+			parent,
+			children: [],
+		};
+	}
+
+	const root: Node = newNode(stack.shift()!);
+	let currentNode: Node = root;
+
+	while (stack.length) {
+		const current = stack.shift()!;
+
+		console.log({ current, currentNode });
+
+		if (current === "(") {
+			if (stack.length && /[A-Z]+/.test(stack[0])) {
+				const route = stack.shift()!;
+				const childNode = newNode(route, currentNode);
+
+				currentNode.children.push(childNode);
+				currentNode = childNode;
+			}
+		} else if (current === ")") {
+			if (currentNode.parent) {
+				currentNode = currentNode.parent;
+			} else {
+				throw Error("No parent found");
+			}
+		} else if (current === "|") {
+			if (stack.length && /[A-Z]+/.test(stack[0])) {
+				const route = stack.shift()!;
+				currentNode = currentNode.parent!;
+				const childNode = newNode(route, currentNode);
+
+				currentNode.children.push(childNode);
+				currentNode = childNode;
+			}
+		} else {
+			currentNode.steps += current.length;
+		}
+	}
+
+	return root;
+}
+function longestPath(tree: Node): number {
+	let path: number = 0;
+
+	function DFS(node: Node) {
+		path = Math.max(node.steps, path);
+
+		for (const child of node.children) {
+			DFS(child);
+		}
+	}
+
+	DFS(tree);
+
+	console.log(tree, path);
+
+	return -1;
+}
