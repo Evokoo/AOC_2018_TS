f38ea07aed31a01949d2c6f524ab72ee92e7c5c9
Evokoo <evanjohncooper@gmail.com>
2025-01-05 04:19:17 +1100

    Day 23 complete

--- 23/23.test.ts
+++ 23/23.test.ts
@@ -6,7 +6,7 @@ import { solveA, solveB } from "./23.ts";
 const currentDay = path.basename(Deno.cwd());
 
 describe(`AOC 2018 - Day ${currentDay}`, () => {
-	describe("Part A", () => {
+	describe.skip("Part A", () => {
 		it("Example", () => {
 			expect(solveA("example_a", currentDay)).toBe(7);
 		});
@@ -22,7 +22,14 @@ describe(`AOC 2018 - Day ${currentDay}`, () => {
 		});
 
 		it("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(0);
+			expect(solveB("input", currentDay)).toBe(88122632);
 		});
 	});
 });
+
+// 32 [ { x: 3095258, y: 27939570, z: 54977234 }, 831, 86012062 ]
+// 64 [ { x: 9499654, y: 28063533, z: 47606827 }, 875, 85170014 ]
+// 80 [ { x: 5671713, y: 25817646, z: 48625697 }, 877, 80115056 ],
+// 128 [ { x: 11748199, y: 27906710, z: 48467723 }, 891, 88122632 ],
+// 100? [ { x: 12216673, y: 29581459, z: 46324500 }, 906, 88122632 ]
+// 128
--- 23/23.ts
+++ 23/23.ts
@@ -1,4 +1,5 @@
 // Imports
+import { BinaryHeap } from "@std/data-structures";
 import TOOLS from "tools";
 
 //Solutions
@@ -10,13 +11,12 @@ export function solveA(fileName: string, day: string): number {
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
 	const nanobots = parseInput(data);
-	return optimalLocation(nanobots);
+	return optimalLocation(getInitialSearchSpace(nanobots), nanobots);
 }
 
 type XYZ = { x: number; y: number; z: number };
 type Nanobot = { id: number; pos: XYZ; radius: number };
-type Space = { min: XYZ; max: XYZ; range: XYZ };
-type RandomPoint = XYZ & { count: number };
+type SearchSpace = { min: XYZ; max: XYZ; count: number; volume: number };
 
 // Functions
 function parseInput(data: string): Nanobot[] {
@@ -43,42 +43,116 @@ function countBotsInRange(nanobots: Nanobot[]): number {
 
 	return count;
 }
-function optimalLocation(nanobots: Nanobot[]) {
-	const { min, max, range } = getSpaceDimension(nanobots);
+function optimalLocation(space: SearchSpace, nanobots: Nanobot[]) {
+	const queue: BinaryHeap<SearchSpace> = new BinaryHeap(
+		(a, b) => b.count - a.count
+	);
 
-	function randomPoint() {
-		const bestPoint = { x: 0, y: 0, z: 0, count: 0 };
+	queue.push(space);
 
-		for (let i = 0; i < 10_000; i++) {
-			const x = Math.floor(Math.random() * (max.x - min.x + 1)) + min.x;
-			const y = Math.floor(Math.random() * (max.y - min.y + 1)) + min.y;
-			const z = Math.floor(Math.random() * (max.z - min.z + 1)) + min.z;
+	let divison = 128;
 
-			const count = inRange(x, y, z, nanobots);
+	while (queue.length) {
+		const { min, max, count, volume } = queue.pop()!;
 
-			if (count > bestPoint.count) {
-				bestPoint.x = x;
-				bestPoint.y = y;
-				bestPoint.z = z;
-				bestPoint.count = count;
+		console.log(divison);
+
+		if (volume <= 1) {
+			return searchSpace({ min, max, count, volume }, nanobots)[0][2];
+		}
+
+		const xIncrement = Math.max(
+			1,
+			Math.floor(Math.abs(max.x - min.x) / divison)
+		);
+		const yIncrement = Math.max(
+			1,
+			Math.floor(Math.abs(max.y - min.y) / divison)
+		);
+		const zIncrement = Math.max(
+			1,
+			Math.floor(Math.abs(max.z - min.z) / divison)
+		);
+
+		const currentRound: BinaryHeap<SearchSpace> = new BinaryHeap(
+			(a, b) => b.count - a.count
+		);
+
+		for (let x = min.x; x < max.x; x += xIncrement) {
+			for (let y = min.y; y < max.y; y += yIncrement) {
+				for (let z = min.z; z < max.z; z += zIncrement) {
+					const newMin = { x, y, z };
+					const newMax = {
+						x: Math.min(x + xIncrement, max.x),
+						y: Math.min(y + yIncrement, max.y),
+						z: Math.min(z + zIncrement, max.z),
+					};
+
+					const centerX = x + Math.floor(xIncrement / 2);
+					const centerY = y + Math.floor(yIncrement / 2);
+					const centerZ = z + Math.floor(zIncrement / 2);
+
+					const count = inRange(centerX, centerY, centerZ, nanobots);
+					const volume = getSearchVolume(newMin, newMax);
+
+					if (count < (currentRound.peek()?.count ?? 0)) {
+						continue;
+					}
+
+					currentRound.push({
+						min: newMin,
+						max: newMax,
+						count,
+						volume,
+					});
+				}
 			}
 		}
 
-		return bestPoint;
+		queue.push(currentRound.pop()!);
+
+		divison = Math.max(2, divison / 2);
 	}
 
-	console.log(randomPoint());
-
-	return -1;
+	throw Error("Point not found");
 }
+function getSearchVolume(min: XYZ, max: XYZ): number {
+	const w = Math.max(1, Math.abs(max.x - min.x));
+	const h = Math.max(1, Math.abs(max.y - min.y));
+	const d = Math.max(1, Math.abs(max.z - min.z));
 
-function getSpaceDimension(nanobots: Nanobot[]): Space {
+	return w * h * d;
+}
+function searchSpace({ min, max }: SearchSpace, nanobots: Nanobot[]) {
+	const points: [XYZ, number, number][] = [];
+
+	for (let x = min.x; x <= max.x; x++) {
+		for (let y = min.y; y <= max.y; y++) {
+			for (let z = min.z; z <= max.z; z++) {
+				const botCount = inRange(x, y, z, nanobots);
+				const distance = TOOLS.manhattanDistance(
+					{ x: 0, y: 0, z: 0 },
+					{ x, y, z }
+				);
+
+				points.push([{ x, y, z }, botCount, distance]);
+			}
+		}
+	}
+
+	return points.sort((a, b) => b[1] - a[1] || a[2] - b[2]);
+}
+function getInitialSearchSpace(nanobots: Nanobot[]): SearchSpace {
 	const min: XYZ = { x: Infinity, y: Infinity, z: Infinity };
 	const max: XYZ = { x: -Infinity, y: -Infinity, z: -Infinity };
+	let trueMax: number = -Infinity;
+	let trueMin: number = Infinity;
 
 	for (const {
 		pos: { x, y, z },
 	} of nanobots) {
+		trueMax = Math.max(...[trueMax, x, y, z]);
+		trueMin = Math.min(...[trueMin, x, y, z]);
 		min.x = Math.min(x, min.x);
 		min.y = Math.min(y, min.y);
 		min.z = Math.min(z, min.z);
@@ -87,9 +161,14 @@ function getSpaceDimension(nanobots: Nanobot[]): Space {
 		max.z = Math.max(z, max.z);
 	}
 
-	const range: XYZ = { x: max.x - min.x, y: max.y - min.y, z: max.z - min.z };
+	return {
+		max: max,
+		min: min,
+		count: 0,
+		volume: getSearchVolume(min, max),
+	};
 
-	return { min, max, range };
+	// return { min, max, count: 0 };
 }
 function inRange(x: number, y: number, z: number, nanobots: Nanobot[]): number {
 	let count = 0;
