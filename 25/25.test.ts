359c8e6d89703bce38e55759ce3351b2e4477349
Evokoo <evanjohncooper@gmail.com>
2025-01-06 02:47:52 +1100

    Day 25 complete

--- 25/25.test.ts
+++ 25/25.test.ts
@@ -1,28 +1,18 @@
 import * as path from "@std/path";
 import { expect } from "@std/expect";
 import { describe, it } from "@std/testing/bdd";
-import { solveA, solveB } from "./25.ts";
+import { solveA } from "./25.ts";
 
 const currentDay = path.basename(Deno.cwd());
 
 describe(`AOC 2018 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		it("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(0);
+			expect(solveA("example_a", currentDay)).toBe(8);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(0);
-		});
-	});
-
-	describe.skip("Part B", () => {
-		it("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(0);
-		});
-
-		it.skip("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveA("input", currentDay)).toBe(346);
 		});
 	});
 });
--- 25/25.ts
+++ 25/25.ts
@@ -4,11 +4,83 @@ import TOOLS from "tools";
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const coordinates = parseInput(data);
+	return countConstellations(coordinates);
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
 	return 0;
 }
 
+type Point_4D = { x: number; y: number; z: number; t: number };
+type Node = {
+	id: string;
+	pos: Point_4D;
+	parent: Node | null;
+	children: Node[];
+};
+
 // Functions
+function parseInput(data: string): Point_4D[] {
+	const coordinates = [];
+
+	for (const line of data.split("\n")) {
+		const [x, y, z, t] = (line.match(/-*\d+/g) || []).map(Number);
+		coordinates.push({ x, y, z, t });
+	}
+
+	return coordinates;
+}
+function countConstellations(coordinates: Point_4D[]) {
+	const connected: Set<string> = new Set();
+
+	function DFS(node: Node): Node {
+		for (const coordinate of coordinates) {
+			const id = pointToId(coordinate);
+			const distance = manhattanDistance_4D(node.pos, coordinate);
+
+			if (!connected.has(id) && distance <= 3) {
+				node.children.push(newNode(coordinate, node));
+				connected.add(id);
+			}
+		}
+
+		for (const child of node.children) {
+			DFS(child);
+		}
+
+		return node;
+	}
+
+	const constellations: Node[] = [];
+
+	for (const coordinate of coordinates) {
+		const root: Node = newNode(coordinate);
+
+		if (!connected.has(root.id)) {
+			connected.add(root.id);
+			constellations.push(DFS(root));
+		}
+	}
+
+	return constellations.length;
+}
+function manhattanDistance_4D(a: Point_4D, b: Point_4D): number {
+	return (
+		Math.abs(a.x - b.x) +
+		Math.abs(a.y - b.y) +
+		Math.abs((a.z ?? 0) - (b.z ?? 0)) +
+		Math.abs((a.t ?? 0) - (b.t ?? 0))
+	);
+}
+function newNode(coordinate: Point_4D, parent: Node | null = null): Node {
+	return {
+		id: pointToId(coordinate),
+		pos: coordinate,
+		parent,
+		children: [],
+	};
+}
+function pointToId({ x, y, z, t }: Point_4D): string {
+	return `${x},${y},${z},${t}`;
+}
